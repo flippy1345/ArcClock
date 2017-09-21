8cefdeeecf10f55baad14b260e2d178bfd60e3ec
flippy1345 <flippy1345@gmail.com>
2017-09-21 12:29:00 +0200

    renamed config to conf created config get the os time and date

--- main.lua
+++ main.lua
@@ -1,11 +1,12 @@
 function love.load(arg)
-
+  screenWith, screenHeight = love.graphics.getDimensions()
 end
 
 function love.update(dt)
-
+  screenWith, screenHeight = love.graphics.getDimensions()
+  osTime = os.date('*t')
 end
 
 function love.draw()
-
+  print("Hour", osTime.hour)
 end
