0db2719416f696d3a6de19fcaa2cdd45ddbf42a4
flippy1345 <flippy1345@gmail.com>
2017-09-21 13:11:55 +0200

    print time

--- main.lua
+++ main.lua
@@ -5,8 +5,9 @@ end
 function love.update(dt)
   screenWith, screenHeight = love.graphics.getDimensions()
   osTime = os.date('*t')
+  love.window.setTitle("ArcClock "..love.timer.getFPS().."FPS")
 end
 
 function love.draw()
-  print("Hour", osTime.hour)
+  print("Hour", osTime.sec)
 end
