f3d7646d685d1aaa700b578e966d40255b9fd03b
flippy1345 <flippy1345@gmail.com>
2017-09-26 09:07:44 +0200

    Added some Shader set origin at height/2 | width/2

--- main.lua
+++ main.lua
@@ -14,9 +14,9 @@ function love.load(arg)
     return r, g, b, a
   end
 
-  function startendDots(radius, sWidth, sHeight, radEnd)
+  function startendDots(radius, xPos, yPos, radEnd)
     local rotatedRadEnd = radEnd-math.rad(90)
-    local centerX, centerY = sWidth*0.5, sHeight*0.5
+    local centerX, centerY = xPos*0.5, yPos*0.5
     local startX, startY = centerX, centerY -radius
     local endX, endY = centerX+radius*math.cos(rotatedRadEnd), centerY+radius*math.sin(rotatedRadEnd)
     -- Start --
@@ -25,26 +25,22 @@ function love.load(arg)
     love.graphics.points(endX, endY)
   end
 
-  function arcClock(radius, sWidth, sHeight, currentPercent, maxPercent, red, green, blue, alpha)
+  function arcClock(radius, xPos, yPos, currentPercent, maxPercent, red, green, blue, alpha)
     local defaultColor = {love.graphics.getColor()}
     local secondToRad = math.rad((360/maxPercent)*currentPercent)
     local redIN, greenIN, blueIN, alphaIN = red or 255, green or 255, blue or 255, alpha or 255
 
-    love.graphics.translate(sWidth/2, sHeight/2)
-    love.graphics.rotate(-math.pi / 2)
-    love.graphics.translate(-sWidth/2, -sHeight/2)
+    love.graphics.rotate(-math.pi*0.5)
     love.graphics.setColor(redIN, greenIN, blueIN, alphaIN)
 
-    love.graphics.arc("line", "open", (sWidth*0.5), (sHeight*0.5), radius, math.rad(0), secondToRad) -- arg #8 : segments
+    love.graphics.arc("line", "open", (xPos*0.5), (yPos*0.5), radius, math.rad(0), secondToRad) -- arg #8 : segments
 
-    love.graphics.translate(sWidth/2, sHeight/2)
     love.graphics.rotate(math.pi*0.5)
-    love.graphics.translate(-sWidth/2, -sHeight/2)
-    startendDots(radius, sWidth, sHeight, secondToRad)
+    startendDots(radius, xPos, yPos, secondToRad)
     love.graphics.setColor(defaultColor)
   end
 
-  function showClock(width, height, time, font, red, green, blue, alpha)
+  function showClock(xPos, yPos, time, font, red, green, blue, alpha)
     local redIN, greenIN, blueIN, alphaIN = red or 255, green or 255, blue or 255, alpha or 255
     local timeData = {
       function() if string.len(time.hour) < 2 then return "0"..time.hour else return time.hour end end,
@@ -56,7 +52,7 @@ function love.load(arg)
 
     love.graphics.setColor(redIN, greenIN, blueIN, alphaIN)
 
-    love.graphics.draw(timeText, (width/2)-(timeText:getWidth()/2) , (height/2)-(timeText:getHeight()/2))
+    love.graphics.draw(timeText, xPos-(timeText:getWidth()/2) , yPos-(timeText:getHeight()/2))
 
     love.graphics.setColor(defaultColor)
   end
@@ -69,8 +65,9 @@ function love.update(dt)
 end
 
 function love.draw()
-  arcClock(200, screenWidth, screenHeight, osTime.sec, 60,  rgb(0, 255, 79)) -- sec
-  arcClock(175, screenWidth, screenHeight, osTime.min, 60, rgb(0, 94, 255)) -- min
-  arcClock(150, screenWidth, screenHeight, osTime.hour % 12, 12, rgb(208, 0, 69)) -- hour
-  showClock(screenWidth, screenHeight, osTime, ubuntuFont, rgb(96, 95, 92))
+  love.graphics.translate(screenWidth/2, screenHeight/2)
+  arcClock(200, 0, 0, osTime.sec, 60,  rgb(0, 255, 79)) -- sec
+  arcClock(175, 0, 0, osTime.min, 60, rgb(0, 94, 255)) -- min
+  arcClock(150, 0, 0, osTime.hour % 12, 12, rgb(208, 0, 69)) -- hour
+  showClock(0, 0, osTime, ubuntuFont, rgb(96, 95, 92))
 end
--- resources/shader/bloom.glsl
+++ resources/shader/bloom.glsl
@@ -1,69 +1,18 @@
-extern vec3 iResolution;
-extern number iTime;
 
-#define saturate(x) (clamp(x, 0., 1.))
+    extern vec2 size = vec2(20,20);
+    extern int samples = 40; // pixels per axis; higher = bigger glow, worse performance
+    extern float quality = .15; // lower = smaller glow, better quality
+    vec4 effect(vec4 colour, Image tex, vec2 tc, vec2 sc){
+		vec4 source = Texel(tex, tc);
+		vec4 sum = vec4(0);
+		int diff = (samples - 1) / 2;
+		vec2 sizeFactor = vec2(1) / size * quality;
 
-
-const number PI = 3.14159;
-
-number blob(vec2 pos, vec2 center, number power)
-{
-    vec2 d = saturate(pow(pos - center, vec2(power)));
-
-    return 1. / abs(d.x + d.y);
-}
-
-vec2 rotate(vec2 p, number t)
-{
-    return mat2(cos(t), -sin(t),
-                  sin(t),  cos(t)) * p;
-}
-
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-	//Get coordinates in the range [-1.0, 1.0]
-    vec2 uv = (fragCoord.xy) / iResolution.xy;
-    uv = (uv - 0.5) * 2.0;
-
-    //Adjust for aspect ratio
-    vec2 aspect_uv = uv * (iResolution.xy / iResolution.y);
-
-
-    number blobs = 0.8 * blob(aspect_uv, vec2(0.), 2.);
-
-    number phi = 0.;
-
-    const int N = 7;
-
-    for (int i = 0; i < N; ++i)
-    {
-        blobs += 0.0001 * blob(rotate(aspect_uv, phi),
-                               vec2(sin(iTime + phi) * 0.64, 0.),
-                               4.);
-
-        blobs += 0.001 * blob(rotate(aspect_uv, phi),
-                              vec2(-sin(iTime + phi + 5. * PI / 6.) * 0.50, 0.),
-                              2.);
-        phi += PI / number(N);
+		for (int x = -diff; x <= diff; x++){
+			for (int y = -diff; y <= diff; y++){
+				vec2 offset = vec2(x, y) * sizeFactor;
+				sum += Texel(tex, tc + offset);
+			}
+		}
+		return ((sum / (samples * samples)) + source) * colour;
     }
-
-    number x = smoothstep(3., 6., blobs);
-    number y = smoothstep(3., 5., blobs);
-    number z = smoothstep(3., 8., blobs);
-
-    fragColor = vec4(2.1, 2., 2.1, 2.) - vec4(x * vec3(1., 0.5, 0.5) +
-                                              y * vec3(0.5, 1., 0.5) +
-                                              z * vec3(0.5, 0.5, 1.), 1.);
-
-
-    fragColor = vec4((1. - vec3(pow(length(uv * 0.4), 1.5))) * vec3(0.9, 1.0, 0.9),
-                     1.) * saturate(pow(fragColor, vec4(2.7)));
-}
-
-
-vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords){
-    vec2 fragCoord = texture_coords * iResolution.xy;
-    mainImage( color, fragCoord );
-    return color;
-}
